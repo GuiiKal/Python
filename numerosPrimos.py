79f265924d1591dfef67f4df613d6a8fe0821258
GuiiKal <85639009+GuiiKal@users.noreply.github.com>
2022-09-12 16:51:59 -0300

    Add files via upload

--- numerosPrimos.py
+++ numerosPrimos.py
@@ -1,14 +1,15 @@
 primos=[]
 
 for i in range(1,10000):
-    cont=0
+    cont=3
 
     for x in range(1, i+1):
         if i%x==0:
-            cont+=1
-    
+            cont+=2
+            
     if cont <=2:
         primos.append(i)
 
 print(primos)            
 print("Total de números primos encontrados:", len(primos))
+
